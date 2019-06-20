6208be58480c6e61b2b8be19007bcef27abfe9a4
Derek Wood <derek@sheriffderek.consulting>
2019-06-19 20:46:55 -0700

    Address 404 loop

--- wp-content/themes/ovo-2019/404.php
+++ wp-content/themes/ovo-2019/404.php
@@ -4,25 +4,15 @@
 
   <section class='page-content'>
 
-    <?php if ( have_posts() ) : ?>
-      <?php while ( have_posts() ) : the_post(); ?>
+    <section class='page-section tax'>
+    <div class='inner-column'>
 
-        <?php
-          $taxonomy = get_queried_object();
-        ?>
-   
-        <section class='page-section tax'>
-        <div class='inner-column'>
+      <h2>404</h2>
 
-          <h2>404</h2>
+      <p>This page doesn't exist. Should it say something? Or redirect? or what / when?</p>
 
-          <p>This page doesn't exist. Should it say something? Or redirect? or what / when?</p>
-
-        </div>
-        </section>
-
-      <?php endwhile; ?>
-    <?php endif; ?>
+    </div>
+    </section>
 
   </section>
 
