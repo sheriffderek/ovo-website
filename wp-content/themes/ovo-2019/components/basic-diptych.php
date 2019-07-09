e006f292c9df6b3377ba2a0f32c3e04212c86113
Derek Wood <derek@sheriffderek.consulting>
2019-07-09 12:13:18 -0700

    Add second image

--- wp-content/themes/ovo-2019/components/basic-diptych.php
+++ wp-content/themes/ovo-2019/components/basic-diptych.php
@@ -3,6 +3,8 @@
   $textContent = get_sub_field('text_content');
   $image = get_sub_field('image_content')['url'];
   $imageAlt = get_sub_field('image_content')['alt'];
+  $secondImage = get_sub_field('second_image')['url'];
+
   $position = get_sub_field('position_type');
   $type = get_sub_field('diptych_type');
   $color = get_sub_field('custom_color');
@@ -16,9 +18,15 @@
 
 
 <aside class='xx <?= $position ?> <?= $type ?>' style='<?= $styles ?>'>
-  <div class='half text'>
-    <?= $textContent ?>
-  </div>
+  <?php if ($secondImage) { ?>
+    <figure class='half'>
+      <img src='<?= $secondImage ?>' alt='<?= $imageAlt ?>' />
+    </figure>
+  <?php } else { ?>
+    <div class='half text'>
+      <?= $textContent ?>
+    </div>
+  <?php } ?>
 
   <figure class='half'>
     <img src='<?= $image ?>' alt='<?= $imageAlt ?>' />
